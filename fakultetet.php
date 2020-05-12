bc866e90fb154976e078d23ba74cf6094b242273
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-05-12 18:12:21 +0200

    Update fakultetet.php

--- fakultetet.php
+++ fakultetet.php
@@ -4,7 +4,7 @@
 	 <meta charset="utf-8">
 	 <meta name="viewport" content="width=device-width">
 	 <link rel="stylesheet" href="reset_P.css"/>
-	 <link rel="stylesheet" href="organet_uni.css"/>
+	 <link rel="stylesheet" href="fakultetet.css"/>
 	  <link rel = "shortcut icon" type = "image/png" href = "llogoja.png">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
