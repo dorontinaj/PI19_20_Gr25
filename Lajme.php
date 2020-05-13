3267faf3901e436e9bded5325fab08ba33f5dce4
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-05-13 09:23:27 +0200

    Ndryshime brenda file-it

--- Lajme.php
+++ Lajme.php
@@ -39,7 +39,309 @@
 }</script>
   
 	</head>
+	<body onload="startTime()">
+	<header> <div id="header">
+	<nav id="mainMenu">
+		 <div><ol>
+        
+           <li><p>Tel: +381 38 244 183 | Mail: <a href="rektorati@uni-pr.edu">rektorati@uni-pr.edu</a></p> </li> </ol> </div>
+           
+             <div class="search-container">
+              <form class="example" action="/action_page.php">
+               <input type="text" placeholder="Kerkoni ketu..." name="search">
+              </form>
+             </div>  
+      </nav>   
+	<nav id="secondaryMenu">
+ <article id="second1">
+ <div id="second1">
+      <h1> <span>Universiteti</span></h1> <br>
+      <h2> Lajmet </h2>
+  </div>
+</article>
+<article id="second2">
+		
+			<a href="projekti.html"  target="_blank">BALLINA</a>
+			<a href="organet_e_universitetit1.html"  target="_blank">ORGANET E UNIVERSITETIT</a>
+			<a href="faqja.html"  target="_blank">FAKULTETET</a>
+			<a href="Lajme.html"  target="_blank">LAJME</a>
+			<a href="alumni.html"  target="_blank">ALUMNI</a>
+			<a href="regjistrimi.html"  target="_blank">REGJISTROHU</a>
+		</article>
+	</nav>
+	</div>
+</header>
+ <div id="ora" style="padding-left:10px;">
+	<script type="text/javascript">
+var now = new Date();
+var hours = now.getHours();
+
+document.write('Ora: ', hours);
+document.bgColor="#2D3E45";
+//6-18 dite
+if (hours > 6 && hours < 18){
+ document.write ('<body style="background-color: #ccc">');
+}
+//17-5 nate
+else if (hours > 18 || hours < 6){
+ document.write ('<body style="background-color: #7D7D7D">');
+}
+</script></div>
+
+   	<section id="lajmetdytesore">
+	<div class="lajmetdytesore">
+	<article>
+	<div id="Lajmi1  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+		"21" Dec
+			
+				</h5>
+				</li>
+			</ul>
+		</div>	
+		<div id="Lajmi1">
+			<figure>
+			<img src="volejboll1.jpg" width="90%" height="35%">
+			</figure>
+			
+			<ol>
+				<li class="Date">21.12.2019</li>
+			</ol>
+			<mark><h3>VOLEJBOLLISTET E UP-SË, FITUESE TË UNIVERSIADËS SË DUBROVNIKUT</h3></mark>
+	<p>Universiteti i Prishtinës “Hasan Prishtina” ka triumfuar në Universiadën e Dubrovnikut, në disiplinën e vjolejbollit për femra, e mbajtur në Kroaci.Ekipi i studenteve të UP-së, me trajner Bahri Gjinovcin, ka mposhtur në finale ekipin e Universitetit të Zagrebit nga Kroacia.Studentet e UP-së kanë qenë të pamposhtura gjatë gjithë universadës, duke mos humbur asnjë set të vetëm në të gjitha fazat e garës. Madje, lojtarja më e mirë e turneut u zgjodh studentja jonë Blinera Jaha.</p>
+    <a href="https://www.uni-pr.edu/page.aspx?id=1,4,985">Lexo me shume...</a>
+		</div>
+		</article>
+	<article>
+	<div id="Lajmi2  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+				
+		"20" Dec
+				</h5>
+			
+				</li>
+			</ul>
+		</div>	
+		<div class="Lajmi2">
+			<figure>
+			<img src="img2.jpg" width="90%" height="35%">
+			</figure>
+			
+			<ol>
+				<li class="Date">20.12.2019</li>
+			</ol>
+			<h3><abbr title="Universiteti i |Prishtines" >UP-JA</abbr> SHPËRBLEN ME MIRËNJOHJE STAFIN DHE STUDENTËT QË NDIHMUAN SHQIPËRINË PAS TËRMETIT</h3>
+	<p>  Fakulteti i Ndërtimtarisë, Fakulteti i Arkitekturës, Departamenti i Psikologjisë së Fakultetit Filozofik, si dhe Parlamenti Studentor i Universitetit të Prishtinës “Hasan Prishtina”, janë shpërblyer me mirënjohje për punën vullnetare të bërë për të prekurit nga tërmeti në Shqipëri.Për këtë kontribut, rektori i UP-së, prof. dr. Marjan Dema tha se i gjithë universiteti ndihet krenar.</p>
+	 <a href="https://www.uni-pr.edu/page.aspx?id=1,4,984">Lexo me shume...</a>
+		</div>
+		
+		</article>
+		<article>
+	<div id="Lajmi3  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+		"18" Dec
+			
+				</h5>
+				</li>
+			</ul>
+		</div>	
+		<div id="Lajmi3">
+			<figure>
+			<img src="img3.jpg" width="90%" height="35%">
+			</figure>
+			
+			<ol>
+				<li class="Date">18.12.2019</li>
+			</ol>
+			<h3>UP-JA CERTIFIKON STUDENTËT PRAKTIKANTË, AMBASADORË SI DHE NDJEKËSIT E TRAJNIMEVE </h3>
+				<p>Studentët e Universitetit të Prishtinës “Hasan Prishtina”, që përfunduan punën praktike me pagesë në njësitë e këtij institucioni, studentët ambasadorë si dhe studentët që kanë ndjekur trajnime, janë certifikuar për punën e tyre.Duke i uruar studentët për punën e tyre, i pari i UP-së, prof. dr. Marjan Dema, tha se shpreson se këta studentë, në një të ardhme të afërt, do të bëhen promotorë të ndryshimeve në vend.</p>
+			<a href="https://www.uni-pr.edu/page.aspx?id=1,4,981">Lexo me shume...</a>
+		</div>
+		</article>
+		
+		
+		
+	</div>
 	
+	<div class="lajmetdytesore">
+	<div>
+	<article>
+	<div id="Lajmi1  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+		"12" Dec
+			
+				</h5>
+				</li>
+			</ul>
+		</div>	
+		<div id="Lajmi1">
+			<figure>
+			<video width="90%" height="35%" controls>
+  <source src="video.mp4" type="video/mp4">
+ 
+</video>
+		
+			</figure>
+			
+			<ol>
+				<li class="Date">12.12.2019</li>
+			</ol>
+			<h3>STUDETET E UNIVERSITETIT PRINTOJN ME 3D PRINTER</h3>
+	<p>Studentet printojn me 3D printer ne universitetin e prishtines.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+	<a href="https://www.youtube.com/watch?v=B8iSyDtWGUQ&list=PL1JZfdUohgO7Qd4P2gH-lsT1y5o9X6-YI&index=9">Lexo me shume...</a>
+		</div>
+		</article>
+	<article>
+	<div id="Lajmi2  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+		"5" Dec
+			
+				</h5>
+				</li>
+			</ul>
+		</div>	
+		<div id="Lajmi2">
+			<figure>
+			<img src="img2.jpg" width="90%" height="35%">
+			</figure>
+			
+			<ol>
+				<li class="Date">05.12.2019</li>
+			</ol>
+			<h3>THIRRJE PËR PJESËMARRJE NË SHKOLLËN DIMËRORE TË BE-SË, NË PRISHTINË
+</h3>
+	<p> Universiteti i Prishtinës “Hasan Prishtina”, në bashkëpunim me European Young Cell Alumni Kosovo dhe projektin Young Cell Scheme, organizon Shkollën Dimërore të Bashkimit Evropian, e cila do të mbahet në amfiteatrin e ri të Fakultetit Filozofik. Shkolla Dimërore Evropiane 2020 do të mbahet ndërmjet datave 3 dhe 6 shkurt 2020.Profesorë nga KU Leuven, Universiteti i Tilburgut, Universiteti i Prishtinës si dhe Alumni Young Cell, të cilët përfunduan studimet e masterit në universitetet prestigjioze të BE-së...</p>
+	<a href="https://www.uni-pr.edu/page.aspx?id=1,4,966">Lexo me shume...</a>
+		</div>
+		</article>
+		</article>
+		<article>
+	<div id="Lajmi3  wraper">
+			<ul id="Lajmi slidebar">
+				<li>
+				<h5>
+		"29" Nov
+			
+				</h5>
+				</li>
+			</ul>
+		</div>	
+		<div id="Lajmi3">
+			<figure>
+			<img src="img3.jpg" width="90%" height="35%">
+			</figure>
+			
+			<ol>
+				<li class="Date">28.11.2019</li>
+			</ol>
+			<h3>KOMUNIKATË E UP-SË NDAJ AKSIONEVE TË SHOQËRISË CIVILE </h3>
+				<p>Menaxhmenti, Kolegjiumi i Dekanëve dhe Parlamenti i Studentëve i Universitetit të Prishtinës “Hasan Prishtina” shpreh shqetësimin për sulmet e paarsyeshme, që po i bëhen në vazhdimësi këtij institucioni. Këto sulme janë të drejtuara kah organet ndër më të rëndësishme, si Senati dhe Këshilli i Etikës. Po ashtu, në vazhdimësi janë sulmuar profesorati dhe menaxhmenti i universitetit.</p>
+				<a href="https://www.uni-pr.edu/page.aspx?id=1,4,963">Lexo me shume...</a>
+		</div>
+		</article>
+		</div>
+		<br><br>
+		<br><br>
+			<div class="pagination">
+  <a onclick="clickCounter()">&laquo;</a>
+  <a href="Lajme.html">1</a>
+  <a href="https://www.uni-pr.edu/page.aspx?id=1,4">2</a>
+  <a href="https://www.uni-pr.edu/page.aspx?id=1,4">3</a>
+  <a href="https://www.uni-pr.edu/page.aspx?id=1,4">4</a>
+  <a href="https://www.uni-pr.edu/page.aspx?id=1,4">5</a>
+  <a href="https://www.uni-pr.edu/page.aspx?id=1,4">6</a>
+  <a href="Lajme.html">&raquo;</a>
+</div>
+		
+<div id="result"></div>
+	</div>
+	
+	</section>
+		
+  <footer>
+<div id="footer">
+
+<nav id="mainFooter">
+<div class="flip"><div class="flip1">Kliko per t'i pare informacionet rreth nesh</div></div>
+<div class="panel">
+	<table class="table2">
+<thead>
+	<tr>
+	<th width="280">TWITTER</th>	
+	<th width="280">INSTAGRAM</th>
+	<th width="280">FACEBOOK</th>
+    <th width="280">KONTAKTI</th>
+</tr>
+
+	</thead>
+
+<tbody>
+<tr>
+	<td>
+<ul style="list-style: none;">
+<li><a href="https://twitter.com/Rektorati">
+				 <span class="ikonat">
+					<i class="fab fa-twitter" style="font-size:40px;color:white" aria-hidden="true"></i>
+				</span> </a></li>
+</ul>
+</td>
+<td>
+<ul style="list-style: none;">
+<li><a href="https://www.instagram.com/p/BzOXH6FhTzR/">
+				 <span class="ikonat">
+					<i class="fab fa-instagram" style="font-size:40px;color:white" aria-hidden="true"></i>
+				</span> </a></li>
+</ul>
+</td>
+<td>
+<ul style="list-style: none;">
+<li><a href="https://www.facebook.com/UniversitetiPrishtines/">
+	<span class="ikonat"> <i class="fab fa-facebook-square" style="font-size:40px;color:white" aria-hidden="true"></i> 
+	</span></a></li>
+</ul>
+</td>
+<td>
+<ul style="list-style: none;">
+<li> <address>
+Adresa:
+Rr."George Bush",Nr.31, 10 000 Prishtine<br>
+ 
+Republika e Kosoves<br> <br> 
+
+Phone: +381 38 244 183<br><br> 
+
+Fax: +381 38 244 187 <br><br> 
+
+Email: <a href="rektorati@uni-pr.edu">rektorati@uni-pr.edu</a><br> 
+
+
+</address> </li>
+
+</ul>
+</td>
+</tbody></table>
+</div>
+</nav>
+<nav id="secondaryFooter">
+	<div id="Copyright"><h6>
+Copyright &copy 2013-All Rights Reserved-Domain Name</h6>
+</div>
+</nav>
+</div>
+</footer>
+        
+		
+	</body>
 	
 	
 
