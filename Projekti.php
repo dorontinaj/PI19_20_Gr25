80cf5da0adb4d9a1f6bd3ea8b5f90e2fcf54d553
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-05-14 21:54:06 +0200

    Update Projekti.php

--- Projekti.php
+++ Projekti.php
@@ -45,5 +45,175 @@ function initMap() {
     
 
 </head>
+	<body><div class="page-wrap">
+<header><div id="header">
+<nav id="mainMenu">
+         <div><ol>
+           <li><p>Tel: +381 38 244 183 | Mail: <a href="rektorati@uni-pr.edu">rektorati@uni-pr.edu</a>
+		</p>
+       	 </li>
+		   </ol></div>
+             <div class="search-container">
+              <form class="example" action="/action_page.php">
+               <input type="text" placeholder="Kerkoni ketu..." name="search">
+              </form>
+             </div>  
+      </nav>   
+ <nav id="secondaryMenu">
+ <article id="second1">
+ <div id="second1">
+      <h1> <span>Universiteti</span></h1> <br>
+      <h2> Ballina </h2>
+      
+     </div>
+	</article>
+	<article id="second2">
+     
+	               <a href="projekti.html"  target="_blank">BALLINA</a>
+			<a href="organet_e_universitetit1.html"  target="_blank">ORGANET E UNIVERSITETIT</a>
+			<a href="faqja.html"  target="_blank">FAKULTETET</a>
+			<a href="Lajme.html"  target="_blank">LAJME</a>
+			<a href="alumni.html"  target="_blank">ALUMNI</a>
+	          	<a href="regjistrimi.html"  target="_blank">REGJISTROHU</a>
+	
+		   </article>
+      </nav>
+	  </div>
+   </header>
+   <div class="Nheader">
+        		 <div class="textbox">
+         <figure id="figura">
+
+           <img src="libra1.jpg" alt="Books" class="center" width="100%" height="100%">
+		    <div class="text-block">
+			<h3>Historia e Universitetit</h3>
+		   <p>Institucioni i parë i arsimit të lartë në Kosovë ishte Shkolla e Lartë Pedagogjike në Prishtinë (1958). Në fillim kishte këto grupe mësimore: Grupi i Gjuhës dhe i Letërsisë Shqipe, Grupi i Gjuhës dhe i Letërsisë Serbokroate, Grupi i Biologjisë, i Gjeografisë dhe i Kimisë, Grupi i BAT-it dhe i Fizikës, Grupi i Matematikës dhe i Fizikës. </p>
+		   <button id="readmore"><a href="https://sq.wikipedia.org/wiki/Universiteti_i_Prishtin%C3%ABs">Lexo me shume</a></button>
+		   </div>
+		   <div class="footerfig">
+		   <article><h2>new 1 </h2>
+		   </article>
+		   <article>
+		   <h2>new 2 </h2>
+		   </article>
+		   <article>
+		   <h2>new 3 </h2>
+		   </article>
+		   </div>
+		   
+         </figure>
+		 </div> 
+      
+	  <article id="mainMain"> 
+	<div class="mainMain1">
+    <article id="part1">
+	<div>
+	<h3>Informacione ndihmese</h3>
+	  <div class="line"></div>
+	<ul>
+<a  href="https://www.uni-pr.edu/page.aspx?id=1,13"><li>Administrata Qendrore
+	<ol id="nest">
+	<li>Arsimin, kërkimet shkencore dhe punën artistike</li>
+<li> Administrimin e njohjes së studimeve</li>
+<li> Administrimin e personelit</li>
+	
+	</ol>
+	</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,7"><li>Zyra per marrdhenie jashte vendit</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,97"><li>Mesimdhenia dhe ceshtjet e studenteve</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,80"><li>Kerkime dhe projekte te sponzorizuara</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,66"><li>Qendra per zhvillim te karrieres
+	<ol id="nest">
+	<li>Informimi</li>
+<li> Këshillimi</li>
+<li>Trajnimet</li>
+	
+	</ol>
+	</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,78"><li>Qendra per persosmeri ne mesimdhenie</li></a>
+	<a  href="https://uni-pr.edu/page.aspx?id=1,90"><li>Publikime shkencore</li></a>
+	
+	</ul>
+	</div><br>
+	<h3>Si te na gjeni</h3>
+      <div class="line"></div>
+    <div id="map"></div>
+	<br>
+	<br>
+	<p>Rr. "George Bush", Nr.31, 10 000 Prishtinë Republika e Kosovës
+	Phone : +381 38 244 183 /244 186
+	Fax : +381 38 244 187 <br>
+	Email : rektorati@uni-pr.edu</p>
+	
+	</article>
+	<article id="part2">
+	<div>
+	<article>
+	<div id="part2.1">
+	<h3>Shiko universitetin</h3>
+	<div class="line"></div>
+	<img src="imagesP/fiek.jpg">
+	<p>Kampusi ka një pozicion vërtet të mirë në qender te Prishtinës me nje hapsir mjaft te madhe ku jane te vendosura shumica e fakulteteve gjithashtu ne qender te kampusit eshte e vendosur bibloteka kombetare.Kjo bibliotekë është institucioni më i lartë bibliotekar në Kosovë i themeluar nga Parlamenti.  </p>
+	<br>
+	<a href="#">Shiko me shume  <i class="arrow right"></i></a>
+	
+	</div>
+	
+	</article>
+	<article>
+	<div id="part22">
+	<h3>Aktivitete sportive</h3>
+	<div class="line"></div>
+	<h5>Dec 21,2019: <span style="color:#2D3E45">Volejboll</span></h5>
+		<img src="volejboll.jpg">
+		<p>Universiteti i Prishtinës “Hasan Prishtina” ka triumfuar në Universiadën e Dubrovnikut, në disiplinën e vjolejbollit për femra, e mbajtur në Kroaci.</p>
+		<div class="line2"></div>
+	<h5>Jan 2,2020: <span style="color:#2D3E45">Basketboll</span></h5>
+		<img src="basketboll.jpg">
+		<p>Prishtina dominon në basketbollin vendor, ngjarjet e vitit 2019 në Kosovë</p>
+		<a href="#">Shiko te gjitha  <i class="arrow right"></i></a>
+		</div>
+	</article>
+	<article>
+	<div id="part23">
+	<h3>Eventet e ardhshme</h3>
+	<div class="line"></div>
+	<h5>APLIKO PËR PUNË PRAKTIKE NË UNINVERSITETIN E PRISHTINËS “HASAN PRISHTINA”</h5>
+	<h4>Jan 2,2020: </h4>
+	<p>Ftohen studentë të UP-së që të aplikojnë për punë praktike (praktikant) në njësitë akademike dhe Administratë qendroretë Universitetittë Prishtinës “Hasan Prishtina “.</p>
+	
+	<div class="line2"></div>
+	<h5>NJOFTIM: FTESË PËR PJESËMARRJE NË PANAIRIN E EDUKIMIT "STUDIONI NË FRANCË"</h5>
+	<h4>Dec 21,2019: </h4>
+	<p>Universiteti i Prishtinës “Hasan Prishtina“ ka nderin t’ju ftojë që të merrni pjesë në edicionin e 2-të të Panairit të Edukimit "Studioni në Francë" 2019. Panairi organizohet me datë 30 tetor 2019 (e mërkurë, nga ora: 09:00 - 17:00 dhe do të mbahet në Hollin e Bibliotekës Kombëtare të Kosovës "Pjetër Bogdani" . 
+</p></div>
+	</article>
+	
+	
+	<div id="part24">
+	
+	<article><h3>Studentet e Diplomuar</h3><div class="line"></div>
+		<img src="diploma.jpg">
+	</article>
+	<article><h3>Mbledhja e Studenteve</h3><div class="line"></div>
+		<img src="mbledhja.jpg">
+	</article>
+	<article><h3>Ligjeratat e Studenteve</h3><div class="line"></div>
+		<img src="ligjerata.jpg">
+	</article>
+	</div>
+	</div>
+
+	</article>
+   </div>
+</article>
+
+
+
+   
+
+</div>
+</div>
+</body>
 
 </html>
