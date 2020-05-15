6be683058f91b409f7d613789b8f8324ceb9b535
Elire Raci <58210135+elire-r@users.noreply.github.com>
2020-05-15 18:27:05 +0200

    Add files via upload
    Rregullimi i faqes Kycu, lidhja me databaze dhe sesionet

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$error = '';
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['pass'])) {
+        $error = "Emri ose fjalekalimi nuk jane ne rregull!";
+    }
+    else {
+        $username = $_POST['username'];
+        $password = $_POST['pass'];
+        $connect = mysqli_connect("localhost", "root", "Elira123-", "regjistrimi");
+        $query = "SELECT username, password FROM perdoruesit WHERE username=? AND password=? LIMIT 1";
+        // Mbrojtja ndaj MySQL injections
+        $s = $connect->prepare($query);
+        $s->bind_param("ss", $username, $password);
+        $s->execute();
+        $s->bind_result($username, $password);
+        $s->store_result();
+        if ($s->fetch())
+        {
+            $_SESSION['login_user'] = $username;
+            header("location: profile.php");
+        }else{
+            $error = "Keni shtypur emrin ose fjalekalimin gabim!";
+        }
+    }
+    mysqli_close($connect);
+}
+?>
