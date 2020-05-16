903ecea73ddecc1e9e990bc818fad14cbaf42539
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-05-17 00:56:39 +0200

    Disa ndryshime ne funksionin per lexim

--- lexo.php
+++ lexo.php
@@ -1,12 +1,9 @@
 <?php
-
-				$file="./newfile.txt";
-				$current= file_get_contents($file);
-				
-				$lines=explode("\n", $current);
-
+				$current = fopen("./newfile.txt", "r") or die("Unable to open file!");
+				$read= fread($current,filesize("newfile.txt"));
+				$lines=explode("\n", $read);
 				foreach($lines as $newline){
 					echo $newline . '<br>';
 				}
-
+				fclose($current);
 ?>
