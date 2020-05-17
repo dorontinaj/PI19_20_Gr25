1c118eca4ce14c04e383977ba18967ea686b6fa2
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-05-17 15:46:55 +0200

    Update lexo.php

--- lexo.php
+++ lexo.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	 <meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width">
+	 <link rel="stylesheet" href="reset_P.css"/>
+	 <link rel="stylesheet" href="css/Lista.css"/>
+	  <link rel = "shortcut icon" type = "image/png" href = "photos/llogoja.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="jquery-1.11.2.min.js"></script>
+<script> 
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+	$("#flip").text("Kliko per ta mbyllur/hapur");
+	
+  });
+});
+</script>
+
+<script> 
 <?php
 				$current = fopen("./newfile.txt", "r") or die("Unable to open file!");
 				$read= fread($current,filesize("newfile.txt"));
