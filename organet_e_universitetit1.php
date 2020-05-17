224f6b4cda5e6f41ec9ad129cd750a6b2c9349e3
Elsa Nishori <40422807+elsa-n@users.noreply.github.com>
2020-05-17 11:46:09 +0200

    Shtimi i require per perfshiren e footerit.

--- organet_e_universitetit1.php
+++ organet_e_universitetit1.php
@@ -128,14 +128,12 @@ else if (hours > 18 || hours < 6){
 	<h4><b>Cilat gjuhë të huaja flisni?</b><h4>
 			
 			<form style="font-size:15px; color:#601A36">
-				
 			<input type="checkbox" name="text" value="Brenda shtetit">Shqip<br>
 			<input type="checkbox" name="text" value="Jashte shtetit">Anglisht<br>
 			<input type="checkbox" name="text" value="Brenda shtetit">Gjermanisht<br>
 			<input type="checkbox" name="text" value="Jashte shtetit">Frengjisht<br>
 			
-			</form>
-
+		</form>
 	<h4><b>Ku deshironi t'i vazhdoni studimet e larta?</b><h4>
 	<form style="font-size:15px; color:#601A36">
 			<input type="radio" name="text" value="Brenda shtetit">Brenda shtetit<br>
@@ -253,68 +251,8 @@ function drop(ev) {
 </tr>
 </table>
 </div>
- 
- <footer>
-<div id="footer">
-<nav id="mainFooter">
-<div class="flip"><div class="flip1">Kliko per t'i pare informacionet rreth nesh</div></div>
-<div class="panel">
-	<table class="table2">
-<thead>
-	<tr>
-	<th width="280">TWITTER</th>	
-	<th width="280">INSTAGRAM</th>
-	<th width="280">FACEBOOK</th>
-    <th width="280">KONTAKTI</th>
-</tr>
-	</thead>
-<tbody>
-<tr>
-	<td>
-<ul style="list-style: none;">
-<li><a href="https://twitter.com/Rektorati">
-				 <span class="ikonat">
-					<i class="fab fa-twitter" style="font-size:40px;color:white" aria-hidden="true"></i>
-				</span> </a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li><a href="https://www.instagram.com/p/BzOXH6FhTzR/">
-				 <span class="ikonat">
-					<i class="fab fa-instagram" style="font-size:40px;color:white" aria-hidden="true"></i>
-				</span> </a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li><a href="https://www.facebook.com/UniversitetiPrishtines/">
-	<span class="ikonat"> <i class="fab fa-facebook-square" style="font-size:40px;color:white" aria-hidden="true"></i> 
-	</span></a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li> 
-<address>
-Adresa: Rr."George Bush",Nr.31, 10 000 Prishtine 
-Republika e Kosoves<br>
-Phone: +381 38 244 183<br>
-Fax: +381 38 244 187 <br>
-Email: <a class="a" href="rektorati@uni-pr.edu">rektorati@uni-pr.edu</a><br> 
-</address> 
-</li>
-</ul>
-</td>
-</tbody></table>
-</div>
-</nav>
-<nav id="secondaryFooter">
-	<div id="Copyright"><h6>
-Copyright &copy 2013-All Rights Reserved-Domain Name</h6>
-</div>
-</nav>
-</div>
-</footer>
+<?php  
+require('footer.php');
+?>
 </body>
 </html>
