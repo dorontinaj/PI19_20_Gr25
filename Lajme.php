37c44ad649bd7d21d7953690226e7ca5eacb20df
Elsa Nishori <40422807+elsa-n@users.noreply.github.com>
2020-05-17 11:45:07 +0200

    Update Lajme.php

--- Lajme.php
+++ Lajme.php
@@ -67,7 +67,7 @@
 			<a href="fakultetet.php"  target="_blank">FAKULTETET</a>
 			<a href="Lajme.php"  target="_blank">LAJME</a>
 			<a href="alumni.php"  target="_blank">ALUMNI</a>
-			<a href="regjistrimi.php"  target="_blank">REGJISTROHU</a>
+			<a href="regjistrohu.php"  target="_blank">REGJISTROHU</a>
 		</article>
 	</nav>
 	</div>
@@ -268,83 +268,8 @@ else if (hours > 18 || hours < 6){
 	</div>
 	
 	</section>
-		
-  <footer>
-<div id="footer">
-
-<nav id="mainFooter">
-<div class="flip"><div class="flip1">Kliko per t'i pare informacionet rreth nesh</div></div>
-<div class="panel">
-	<table class="table2">
-<thead>
-	<tr>
-	<th width="280">TWITTER</th>	
-	<th width="280">INSTAGRAM</th>
-	<th width="280">FACEBOOK</th>
-    <th width="280">KONTAKTI</th>
-</tr>
-
-	</thead>
-
-<tbody>
-<tr>
-	<td>
-<ul style="list-style: none;">
-<li><a href="https://twitter.com/Rektorati">
-				 <span class="ikonat">
-					<i class="fab fa-twitter" style="font-size:40px;color:white" aria-hidden="true"></i>
-				</span> </a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li><a href="https://www.instagram.com/p/BzOXH6FhTzR/">
-				 <span class="ikonat">
-					<i class="fab fa-instagram" style="font-size:40px;color:white" aria-hidden="true"></i>
-				</span> </a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li><a href="https://www.facebook.com/UniversitetiPrishtines/">
-	<span class="ikonat"> <i class="fab fa-facebook-square" style="font-size:40px;color:white" aria-hidden="true"></i> 
-	</span></a></li>
-</ul>
-</td>
-<td>
-<ul style="list-style: none;">
-<li> <address>
-Adresa:
-Rr."George Bush",Nr.31, 10 000 Prishtine<br>
- 
-Republika e Kosoves<br> <br> 
-
-Phone: +381 38 244 183<br><br> 
-
-Fax: +381 38 244 187 <br><br> 
-
-Email: <a href="rektorati@uni-pr.edu">rektorati@uni-pr.edu</a><br> 
-
-
-</address> </li>
-
-</ul>
-</td>
-</tbody></table>
-</div>
-</nav>
-<nav id="secondaryFooter">
-	<div id="Copyright"><h6>
-Copyright &copy 2013-All Rights Reserved-Domain Name</h6>
-</div>
-</nav>
-</div>
-</footer>
-        
-		
+	<?php
+	require('footer.php');
+	?>
 	</body>
-	
-	
-
-
 </html>
