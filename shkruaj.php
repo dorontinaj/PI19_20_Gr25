a17d4a27ba988ea79e73849b25c3b33185ac49a8
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-05-17 20:58:29 +0200

    Delete shkruaj.php

--- shkruaj.php
+++
@@ -1,9 +0,0 @@
-<?php
-
- 				$myfile = fopen("./newfile.txt", "a") or die("Unable to open file!");
-				$txt = htmlspecialchars($_POST['krit'], ENT_QUOTES, 'UTF-8');
-				$txtt = $txt . PHP_EOL;
-				fwrite($myfile, $txtt);
-				fclose($myfile);
-				echo "Faleminderit qe kontribuat!";
-?>
